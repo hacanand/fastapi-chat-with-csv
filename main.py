05c4a468f364e6eab16f52cecced97d0c7bac532
anand <anandpromax@gmail.com>
2024-05-27 21:41:50 +0530

    chore: Remove unnecessary line and update dependencies and configuration files

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import uvicorn
 from fastapi import FastAPI, Request
 from langchain_experimental.agents.agent_toolkits import create_csv_agent
 from langchain_community.chat_models import ChatOpenAI
@@ -6,6 +7,7 @@ from dotenv import load_dotenv
 load_dotenv()
 # from pydantic import BaseModel
 
+ 
 
 app = FastAPI()
  
@@ -16,7 +18,9 @@ async def test():
 @app.get("/{query}")
 async def chat(query: str):
         llm=ChatOpenAI(temperature=0, model="gpt-3.5-turbo-0613", openai_api_key= os.environ["OPENAI_API_KEY"])
-        agent_executer=create_csv_agent(llm, 'salaries.csv', verbose=True,return_intermediate_steps=True)
-        response = agent_executer.invoke(query)
+        agent_executer=  create_csv_agent(llm, 'salaries.csv', verbose=True,return_intermediate_steps=True)
+        response =  agent_executer.invoke(query)
         return response
-     
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
