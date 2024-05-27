94a11dd0cc234e0980814f2cdd0c79c376c1a05f
anand <anandpromax@gmail.com>
2024-05-27 19:03:23 +0530

    fastapi_chat_with_csv

---
+++ main.py
@@ -0,0 +1,21 @@
+import os
+from fastapi import FastAPI, Request
+from langchain_experimental.agents.agent_toolkits import create_csv_agent
+from langchain_community.chat_models import ChatOpenAI
+from dotenv import load_dotenv
+load_dotenv()
+# from pydantic import BaseModel
+
+ 
+
+app = FastAPI()
+ 
+
+@app.get("/{query}")
+async def chat(query: str):
+        llm=ChatOpenAI(temperature=0, model="gpt-3.5-turbo-0613", openai_api_key= os.environ["OPENAI_API_KEY"])
+        agent_executer=create_csv_agent(llm, 'salaries.csv', verbose=True,return_intermediate_steps=True)
+        response = agent_executer.invoke(query)
+
+        return response
+     
