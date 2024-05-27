956d2222e3848b28b86c7f51b61538fc74656f2c
anand <anandpromax@gmail.com>
2024-05-27 20:13:53 +0530

    Update main.py to remove unnecessary line

--- main.py
+++ main.py
@@ -18,6 +18,5 @@ async def chat(query: str):
         llm=ChatOpenAI(temperature=0, model="gpt-3.5-turbo-0613", openai_api_key= os.environ["OPENAI_API_KEY"])
         agent_executer=create_csv_agent(llm, 'salaries.csv', verbose=True,return_intermediate_steps=True)
         response = agent_executer.invoke(query)
-
         return response
      
